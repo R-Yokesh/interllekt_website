644351b35e04259d5d82581ddb5ecccd0731ee25
karto-pyaswanth <yaswanth.p@kartoffel.in>
2024-07-01 17:34:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Section2 from './components/Capabilities/Section2/Section2';
 import Card from './components/Capabilities/Card/Card';
 import Testiomonial from './components/Capabilities/Testiomonial/Testiomonial';
 import ContactForm from './components/Capabilities/ContactForm/ContactForm';
+import BCard from './components/Business/BCard/BCard';
+import BContactForm from './components/Business/BContactForm/BContactForm';
+import BSection2 from './components/Business/BSection2/BSection2';
+import BSlider from './components/Business/BSlider/BSlider';
+import BSlider2 from './components/Business/BSlider2/BSlider2';
+import BTestiomonial from './components/Business/BTestiomonial/BTestiomonial';
 
 
 
@@ -19,6 +25,18 @@ function App() {
       <Slider2 />
       <Testiomonial />
       <ContactForm />
+
+  {/* Business */}
+
+  <BSection2 />
+  <BSlider />
+  <BCard />
+  <BSlider2 />
+  <BTestiomonial />
+  <BContactForm />
+
+  
+
       
     </div>
   );
