6e39b66e704721e8f3395e4fba137c4408c54db3
karto-yokesh <yokesh.r@kartoffel.in>
2024-07-02 12:15:26 +0530

    changes

--- src/Assets/Assets.jsx
+++ src/Assets/Assets.jsx
@@ -1,8 +1,14 @@
 import logo from "./Images/logoblue.png"
 import logoo from "./Images/Mark-12.png"
+import searchicon from "./Images/search-icon.png"
+import interllektlogo from "./Images/interllekt-logo.png"
+import menuicon from "./Images/menu-icon.png"
 
 const Assets ={
     logo:logo,
-    logoo
+    logoo,
+    searchicon,
+    interllektlogo,
+    menuicon,
 }
 export default Assets;
