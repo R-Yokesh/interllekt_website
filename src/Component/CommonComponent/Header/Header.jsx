11f44f4088f9f4d51d39681b24ace44f059dab09
karto-pyaswanth <yaswanth.p@kartoffel.in>
2024-07-02 13:18:55 +0530

    path image changes

--- src/Component/CommonComponent/Header/Header.jsx
+++ src/Component/CommonComponent/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import Assets from "../../../Assets/Assets"
+import Assets from "../../../assets/Assets"
 
 import "./Header.css";
 
