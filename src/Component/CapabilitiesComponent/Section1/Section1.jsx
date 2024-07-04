3e857ac07ea856b1ffe37d514f7cc799ab937834
karto-yokesh <yokesh.r@kartoffel.in>
2024-07-04 19:57:15 +0530

    EOD

--- src/Component/CapabilitiesComponent/Section1/Section1.jsx
+++ src/Component/CapabilitiesComponent/Section1/Section1.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Section1.css';
 
-
 const Section1 = () => {
+  const [displayText, setDisplayText] = useState('Capabilities');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayText((prevText) => (prevText === 'Capabilities' ? 'Core' : 'Capabilities'));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="section1-container">
       <div className="section1-body">
-      <h1 class="heading-style-h1">Our Core<br/><span class="text-style-italic animated-text">Capabilities</span></h1>
-      <div className='para'>
-        <p>Interllekt is decades of collective expertise to help individuals & organisations achieve transformation and growth, translating concepts into enduring branding and legacies. Embracing a global perspective towards an impactful future.</p>
-      </div>
+        <h1 className="heading-style-h1">
+          Our <br />
+          <span className="text-style-italic animated-text">{displayText}</span>
+        </h1>
+        <div className="para">
+          <p>
+            Interllekt is decades of collective expertise to help individuals & organisations achieve transformation and growth, translating concepts into enduring branding and legacies. Embracing a global perspective towards an impactful future.
+          </p>
+        </div>
       </div>
     </div>
   );
