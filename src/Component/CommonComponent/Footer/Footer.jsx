e1db1ba41a5c7d9cdbbca64ce485e2fdb88a86a8
karto-pyaswanth <yaswanth.p@kartoffel.in>
2024-07-09 12:47:56 +0530

    changes

--- src/Component/CommonComponent/Footer/Footer.jsx
+++ src/Component/CommonComponent/Footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import Assets from '../../../Assets/Assets';
 
 const Footer = () => {
@@ -22,44 +20,33 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-2 col-md-6 logcnm">
-            <img src={Assets.logo} alt="Logo" style={{ width: "120px" }} />
+            <img src={Assets.interllektlogo} alt="Logo" style={{ width: "100px" }} />
+            {/* <span>Prosperity in Motion</span> */}
           </div>
           <div className="col-lg-3 col-md-6 logcnm titls">
             <ul className="list-unstyled">
-            <li className='folin'><Link to="/" onClick={() => console.log('Navigating to Home')}>Home</Link></li>
+              <li className='folin'><Link to="/" onClick={() => console.log('Navigating to Home')}>Home</Link></li>
               <li className='folin'><Link to="/" onClick={() => console.log('Navigating to About')}>About</Link></li>
-              <li className='folin'><Link to="/" onClick={() => scrollToSection('contact') }>Contact</Link></li>
+              <li className='folin'><Link to="/" onClick={() => scrollToSection('contact')}>Contact</Link></li>
               <li className='folin'><Link to="/Capabilities" onClick={() => console.log('Navigating to Capabilities')}>Capabilities</Link></li>
               <li className='folin'><Link to="/PrivacyAndPolicy" onClick={() => console.log('Navigating to Privacy and Policy')}>Privacy And Policy</Link></li>
             </ul>
           </div>
-          <div className="col-lg-3 col-md-6 social-section logcnm" >
-            <h4>Socials</h4>
-            <div className="social-icons">
-              <a href="https://www.instagram.com/interllekt" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
-              </a>
-              <a href="https://twitter.com/interllekt" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </a>
-              <a href="https://www.facebook.com/interllekt" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
-              </a>
-              <a href="https://www.linkedin.com/company/interllekt" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-              </a>
-            </div>
+          <div className="col-lg-3 col-md-6 logcnm">
+            <h5>Follow us</h5>
+            <ul className="list-unstyled">
+              <li><a href="https://www.linkedin.com/company/interllekt" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+              <li><a href="https://twitter.com/interllekt" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+              <li><a href="https://www.facebook.com/interllekt" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+              <li><a href="https://www.instagram.com/interllekt" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            </ul>
           </div>
-          <div className="col-lg-4 col-md-6 logcnm">
-            <h4 className='sgh'>Subscribe</h4>
-            <form className="form-subscribe" onSubmit={handleSubmit}>
-              <div className="input-group">
-                <input type="email" className="form-control input-lg" placeholder="Your email address" required />
-                <span className="input-group-btn">
-                  <button className="btn btn-success btn-lg" type="submit">Subscribe</button>
-                </span>
-              </div>
-            </form>
+          <div className="col-lg-3 col-md-6 logcnm">
+            <h5>Offices</h5>
+            <ul className="list-unstyled">
+              <li>Menachem Begin 144, Tel Aviv</li>
+              <li>215 Park Ave South, New York</li>
+            </ul>
           </div>
         </div>
         <div className="row">
