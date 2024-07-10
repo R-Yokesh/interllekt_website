375c1e338bd8f55f10bd55b57b830ae76b166a0c
karto-yokesh <yokesh.r@kartoffel.in>
2024-07-10 13:16:34 +0530

    changes

--- src/Component/PrivacyPolicyComponent/PrivacyAndPolicy.jsx
+++ src/Component/PrivacyPolicyComponent/PrivacyAndPolicy.jsx
@@ -6,7 +6,7 @@ const PrivacyAndPolicy = () => {
     <div className="privacy-container">
       <h1>Privacy Policy</h1>
       <p>
-        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+        This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of your information when you use the Service and tells you about Your privacy rights and how the law protects you.
       </p>
       <p>
         We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Privacy Policy Generator.
