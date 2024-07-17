b65b6f767355c0add9f1441762221a036bf3d78e
karto-yokesh <yokesh.r@kartoffel.in>
2024-07-17 15:30:44 +0530

    site changes

--- src/Component/HomePageComponent/AnimationVideo/AnimationVideo.jsx
+++ src/Component/HomePageComponent/AnimationVideo/AnimationVideo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './AnimationVideo.css';
-import video from '../../../Assets/video.mp4'
+import video from '../../../Assets/WebsiteVideo.webm'
 
 const AnimationVideo = () => {
   return (
     <div className="video-container">
       <video className="full-screen-video" autoPlay loop muted>
-        <source src={video} type="video/mp4" />
+        <source src={video} type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
