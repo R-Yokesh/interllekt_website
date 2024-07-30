519434ff9e67dbc8175bfd28a72c0a89b9d67fb9
karto-yokesh <yokesh.r@kartoffel.in>
2024-07-30 20:00:31 +0530

    site changes eod

--- src/Component/HomePageComponent/CardContent/CardContent.jsx
+++ src/Component/HomePageComponent/CardContent/CardContent.jsx
@@ -7,6 +7,7 @@ import image3 from '../../../Assets/Images/Color Mark 1.png';
 import image4 from '../../../Assets/Images/Color Mark 2.png';
 import image5 from '../../../Assets/Images/Color Mark 3.png';
 import image6 from '../../../Assets/Images/Color Mark 4.png';
+import image7 from '../../../Assets/Images/Color Mark 3.png';
 
 const CardContent = () => {
   const [focusedCard, setFocusedCard] = useState('Capabilities');
@@ -36,44 +37,53 @@ const CardContent = () => {
       <div className="col" style={{ backgroundImage: `url(${image4})` }}></div>
       <div className="col" style={{ backgroundImage: `url(${image5})` }}></div>
       <div className="col" style={{ backgroundImage: `url(${image6})` }}></div>
+      <div className="col" style={{ backgroundImage: `url(${image7})` }}></div>
     </div>
   );
 
   return (
-    <div className="card-content">
-      <div className="left-side">
-        <Link to="/capabilities" className={`card ${focusedCard === 'Capabilities' ? 'focused' : ''}`} data-card="Capabilities">
-          <h1>Capabilities</h1>
-          {focusedCard === 'Capabilities' && renderMasonryGrid()}
-        </Link>
-        <Link to="/innovation" className={`card ${focusedCard === 'Innovation' ? 'focused' : ''}`} data-card="Innovation">
-          <h1>Innovation</h1>
-          {focusedCard === 'Innovation' && renderMasonryGrid()}
-        </Link>
-        <Link to="/business" className={`card ${focusedCard === 'Business' ? 'focused' : ''}`} data-card="Business">
-          <h1>Business</h1>
-          {focusedCard === 'Business' && renderMasonryGrid()}
-        </Link>
-      </div>
-      <div className="right-side">
-        {focusedCard === 'Capabilities' && (
-          <div className="c-content cap-content">
-            <h2>Capabilities</h2>
-            <p>A visionary creative network striving to craft sublime communication through design, pushing boundaries, we boldly explore new horizons, unlocking limitless possibilities through creative expression.</p>
+    <div className="container">
+      <div className="card-content row">
+        <div className="sleft-side col-lg-6">
+          <div className="capclass">
+            <Link to="/capabilities" className={`card ${focusedCard === 'Capabilities' ? 'focused' : ''}`} data-card="Capabilities">
+              <div><h1>Capabilities</h1></div>
+            </Link>
+            {focusedCard === 'Capabilities' && renderMasonryGrid()}
           </div>
-        )}
-        {focusedCard === 'Innovation' && (
-          <div className="c-content i-content">
-            <h2>Innovation</h2>
-            <p>We built an environment where every idea is valued and explored. When staying ahead of the curve or finding a way to make it work are a system of belief that we foster to help at any stage of an organisation. Whether it is digital transformation or creating a product with impact we explore and push boundaries.</p>
+          <div className="innoclass">
+            <Link to="/innovation" className={`card ${focusedCard === 'Innovation' ? 'focused' : ''}`} data-card="Innovation">
+              <div><h1>Innovation</h1></div>
+            </Link>
+            {focusedCard === 'Innovation' && renderMasonryGrid()}
           </div>
-        )}
-        {focusedCard === 'Business' && (
-          <div className="c-content b-content">
-            <h2>Business</h2>
-            <p>Crafting processes that help transform our partner organisation to be future-proof. With Strategy together with an approach so bold that ideas become Enterprise and transform them into truly global entities forging the way ahead with impact.</p>
+          <div className="buiclass">
+            <Link to="/business" className={`card ${focusedCard === 'Business' ? 'focused' : ''}`} data-card="Business">
+              <div><h1>Business</h1></div>
+            </Link>
+            {focusedCard === 'Business' && renderMasonryGrid()}
           </div>
-        )}
+        </div>
+        <div className="sright-side col-lg-6">
+          {focusedCard === 'Capabilities' && (
+            <div className="c-content cap-content">
+              <h2>Capabilities</h2>
+              <p>A visionary creative network striving to craft sublime communication through design, pushing boundaries, we boldly explore new horizons, unlocking limitless possibilities through creative expression.</p>
+            </div>
+          )}
+          {focusedCard === 'Innovation' && (
+            <div className="c-content i-content">
+              <h2>Innovation</h2>
+              <p>We built an environment where every idea is valued and explored. When staying ahead of the curve or finding a way to make it work are a system of belief that we foster to help at any stage of an organisation. Whether it is digital transformation or creating a product with impact we explore and push boundaries.</p>
+            </div>
+          )}
+          {focusedCard === 'Business' && (
+            <div className="c-content b-content">
+              <h2>Business</h2>
+              <p>Crafting processes that help transform our partner organisation to be future-proof. With Strategy together with an approach so bold that ideas become Enterprise and transform them into truly global entities forging the way ahead with impact.</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
