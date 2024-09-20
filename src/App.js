f729cb0951f90a60abc504691da50cd122bfcf37
karto-pyaswanth <yaswanth.p@kartoffel.in>
2024-09-20 17:10:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
       {/* <RoutesComponent /> */}
       <ErrorBoundary>
-        <Router basename='/interllekt.com'>
+        <Router basename='/'>
           <ScrollToTop />
           <Routes>
             <Route path='/*' element={<Layout />} />
